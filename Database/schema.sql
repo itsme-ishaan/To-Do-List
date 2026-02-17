eb9d3519858d24bf6d086b0945d225aaff9c7979
Ishaan Gaur <123890195+itsme-ishaan@users.noreply.github.com>
2026-02-17 21:20:58 +0530

    Update schema.sql

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,1 +1,22 @@
+CREATE DATABASE student_task_db;
+USE student_task_db;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    title VARCHAR(255),
+    description TEXT,
+    status VARCHAR(20),
+    due_date DATE,
+    created_at TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
 
